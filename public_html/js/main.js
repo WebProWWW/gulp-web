310356100043c050bd314a33e8d9b7052f8ed153
Тимур <v.timur8484@gmail.com>
2020-06-20 00:07:42 +0500

    delete public css js

--- public_html/js/main.js
+++
@@ -1,5 +0,0 @@
-/*!
- * @author Timur Valiyev
- * @link https://webprowww.github.io
- */
-"use strict";function _classCallCheck(n,a){if(!(n instanceof a))throw new TypeError("Cannot call a class as a function")}(function(){}).call(void 0);
