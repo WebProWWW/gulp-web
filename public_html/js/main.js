3e334fa9184e018a810d1a04b73744fe1a57858b
Tim <v.timur8484@gmail.com>
2022-05-21 01:05:51 +0500

    Add ignored public assets

---
+++ public_html/js/main.js
@@ -0,0 +1,18 @@
+/*!
+ * @author Timur Valiyev
+ * @link https://webprowww.github.io
+ */
+"use strict";
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+  var MyClass;
+  MyClass = /*#__PURE__*/_createClass(function MyClass() {
+    _classCallCheck(this, MyClass);
+  });
+}).call(void 0);
