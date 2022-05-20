65ff55e70650ef8b6c5dfe43fb4f022850718c00
Tim <v.timur8484@gmail.com>
2022-05-21 00:56:39 +0500

    Inline js css on dev

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,47 @@
 <?php
 
-$dev = true;
-$v = 1;
+define('IS_DEV', 1);
+define('ASSET_V', '1');
 
-if ($dev) $v = $v . 'r' . time();
+class App {
+
+    public function css() {
+        return ''
+            .$this->getStyle('css/depends.css')
+            .$this->getStyle('css/main.css')
+        .'';
+    }
+
+    public function js() {
+        return ''
+            .$this->getScript('js/depends.js')
+            .$this->getScript('js/main.js')
+            .$this->getScript('vue/app.js')
+        .'';
+    }
+
+    private function getStyle($file) {
+        $out = '';
+        if (IS_DEV) {
+            $out = '<style>' . file_get_contents(__DIR__ . '/' . $file) . '</style>';
+        } else {
+            $out = '<link rel="stylesheet" href="' . $file . '?v=' . ASSET_V . '">';
+        }
+        return $out;
+    }
+
+    private function getScript($file) {
+        $out = '';
+        if (IS_DEV) {
+            $out = '<script>' . file_get_contents(__DIR__ . '/' . $file) . '</script>';
+        } else {
+            $out = '<script src="' . $file . '?v=' . ASSET_V . '"></script>';
+        }
+        return $out;
+    }
+}
+
+$app = new App;
 
 ?>
 <!DOCTYPE html>
@@ -15,20 +53,17 @@ if ($dev) $v = $v . 'r' . time();
     <meta name="format-detection" content="address=no">
     <meta name="format-detection" content="email=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
-    <link rel="stylesheet" href="css/depends.css?v=<?= $v ?>">
-    <link rel="stylesheet" href="css/main.css?v=<?= $v ?>">
+    <?= $app->css() ?>
     <title>New Project</title>
 </head>
 <body>
 
-<div class="container">
-    <a href="https://github.com/WebProWWW/gulp-web" target="_blank">README</a>
+<div class="container pt-3">
+    <p><a href="https://github.com/WebProWWW/gulp-web" target="_blank">README</a></p>
 </div><!-- .container -->
 <div class="js-vue-app"></div>
 
-<script src="js/depends.js?v=<?= $v ?>"></script>
-<script src="js/main.js?v=<?= $v ?>"></script>
-<script src="vue/app.js?v=<?= $v ?>"></script>
+<?= $app->js() ?>
 
 </body>
 </html>
